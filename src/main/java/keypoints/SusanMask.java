d42c83960adbf4ffad60bf1e6176eab09baa325a
administrator <fernando.en.caballito@gmail.com>
2016-10-07 18:01:40 -0300

    empezando susan

--- src/main/java/keypoints/SusanMask.java
+++ src/main/java/keypoints/SusanMask.java
@@ -1,5 +1,7 @@
 package keypoints;
 
+import java.awt.image.BufferedImage;
+
 /*
 * @author Fernando Bejarano
 */
@@ -12,7 +14,9 @@ public class SusanMask {
 	private static int SIZE = 7;
 	private static int HALF = (SIZE - 1) / 2;
 	private static int MASK_PIXELS = 36;
-
+	private static int THRESHOLD=27;//umbral que se usa para comparar niveles de gris con el núcleo en la máscara.
+	
+	
 	// devuelve elementos de la imagen que caen dentro de la máscara
 	// recibe la posición en la que se ubica el centro de la máscara respecto de
 	// la imagen
@@ -39,8 +43,67 @@ public class SusanMask {
 		return ans;
 	}
 
-	public BufferedImage getBordersAndCorners(int[][] originalImage) {
+	public BufferedImage getBordersAndCorners(BufferedImage originalImage) {
+		int width = originalImage.getWidth();
+		int height = originalImage.getHeight();
+		BufferedImage result = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+		
+		int [][]original_image_matrix=new int[height][width];
+		
+		//se copia imagen original
+		for (int i = 0; i < width; i++) {
+			for (int j = 0; j < height; j++) {
+				int color=originalImage.getRGB(i, j);
+				result.setRGB(i, j,color);
+				original_image_matrix[j][i]=color;
+				}
+		}
+	
+		
+		//se itera por la imagen original
+		
+		for(int row=HALF;row<height;row++){
+			for(int col=HALF;col<width;col++){
+				int current_pixel=result.getRGB(col, row);
+				//se obtienen los elementos que caen dentro de la máscara
+				int [] elements_in_mask=getElementsInMask(col, row, original_image_matrix);
+				
+				
+				//conteo de pixeles con mismo nivel de gris
+				int same_grey_count=getSameGreyCount(elements_in_mask,current_pixel, THRESHOLD);
+				
+				
+				//calculo de s
+				int s=1-same_grey_count/MASK_PIXELS;
+				
+				
+				//se evalua s
+				
+				evaluateBordersAndCorners(row,col,s,result);
+				
+				
+			}
+		}
+		
+		
+		
 		return null;
+	}
+
+	private void evaluateBordersAndCorners(int row, int col, int s, BufferedImage result) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public int getSameGreyCount(int[] elements_in_mask, int current_pixel,int threshold) {
+		
+		int count=0;
+		for(int elem:elements_in_mask){
+			if(Math.abs(elem-current_pixel)<threshold)
+				count++;
+		}
+		
+		return count;
 	}
 
 }
